1d22d1299f1a9552ee018193081e64f357c13bed
Step <kononov_stepan@mail.ru>
2026-06-14 14:59:41 +0300

    add settings to model and DBcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<VideoStream> Cameras => Set<VideoStream>();
+}
