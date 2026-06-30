d80c1c805b6ff1fb7fe01b1d5f8723d87a2764ca
Stepan <kononov_stepan@mail>
2026-06-30 12:04:59 +0000

    fix

--- Service/Image/FileImageStorage.cs
+++ Service/Image/FileImageStorage.cs
@@ -8,7 +8,26 @@ public class FileImageStorage : IImageStorage
             "PlateImages",            
             now.Year.ToString(),
             now.Month.ToString("D2"),
-            now.Day.ToString("D2")
+            now.Day.ToString("D2"), "plate"
+        );
+
+        Directory.CreateDirectory(dir);
+        var fileName = $"{cameraId}_{plate}_{now:HHmmss}.jpg";
+        var fullPath = Path.Combine(dir, fileName);
+        
+        await File.WriteAllBytesAsync(fullPath, jpg);
+        return fullPath;
+    }
+
+    public async Task<string> SaveFrameAsync(byte[] jpg, string plate, int cameraId)
+    {        
+        var now = DateTime.UtcNow;
+
+        var dir = Path.Combine(
+            "PlateImages",            
+            now.Year.ToString(),
+            now.Month.ToString("D2"),
+            now.Day.ToString("D2"), "frame"
         );
 
         Directory.CreateDirectory(dir);
